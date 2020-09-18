d70f16ba6b6aa5929e49050a5b5b32f996464ece
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 06:56:47 +0700

    add readableDate filter

--- src/plugins/filters.js
+++ src/plugins/filters.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import dayjs from 'dayjs'
 
 Vue.filter('capitalize', val => {
   return val.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1)).join(' ')
 })
 Vue.filter('currency', val => new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(val))
+Vue.filter('readableDate', val => dayjs(val).format('DD MMMM, YYYY'))
