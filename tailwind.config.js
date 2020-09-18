cd367e1660a67b96c8c5c4efb42e2aff018fc5cc
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-18 13:51:54 +0700

    extends color half, dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        half: 'rgba(0, 0, 0, .24)'
+        half: 'rgba(0, 0, 0, .24)',
+        dark: 'rgba(0, 0, 0, .45)'
       },
       fontFamily: {
         nunito: ['nunito']
