493d1c8c1d2b8a432f2ee324973ccd49e0d6c720
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-18 17:24:21 +0700

    vue-progressbar addedd

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import VueTailwind from 'vue-tailwind'
 import Transitions from 'vue2-transitions'
 import VueSweetAlert2 from 'vue-sweetalert2'
+import VueProgressBar from 'vue-progressbar'
+
+const options = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false
+}
 
 const tailwindSetting = {}
 
+Vue.use(VueProgressBar, options)
 Vue.use(VueTailwind, tailwindSetting)
 Vue.use(Transitions)
 Vue.use(VueSweetAlert2)
