92822cd73653ae9c960f5c6ea0b1d857607d6ab3
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-18 15:49:52 +0700

    use sweetalert plugin globally

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueTailwind from 'vue-tailwind'
 import Transitions from 'vue2-transitions'
+import VueSweetAlert2 from 'vue-sweetalert2'
 
 const tailwindSetting = {}
 
 Vue.use(VueTailwind, tailwindSetting)
 Vue.use(Transitions)
+Vue.use(VueSweetAlert2)
