18d53903853a1d779be6aa2942774de1dd29eb24
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-18 13:52:29 +0700

    generate formgroup component

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="flex flex-col mb-5 justify-start items-center w-full">
+      <label class="w-full mb-3">{{ label }}</label>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['label']
+}
+</script>
--- src/plugins/globalComponent.js
+++ src/plugins/globalComponent.js
@@ -1,3 +1,4 @@
 import Vue from 'vue'
 
 Vue.component('base-card', () => import(/* webpackChunkName: 'card-component' */'../components/Card.vue'))
+Vue.component('form-group', () => import(/* webpackChunkName: 'form-group' */'../components/FormGroup.vue'))
