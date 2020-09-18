c7eb47088190033622ca0c8478d4403795fae2a9
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-18 15:50:55 +0700

    add form to login page

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -9,30 +9,55 @@
           Login
         </h2>
       </template>
-      <form-group
-        label="Username"
-      >
-        <input v-model="form.username" type="text" class="bg-dark appearance-none rounded w-full py-2 px-4 text-gray-100 leading-tight focus:outline-none" placeholder="Username">
-      </form-group>
-      <form-group
-        label="Password"
-      >
-        <input v-model="form.password" type="password" class="bg-dark appearance-none rounded w-full py-2 px-4 text-gray-100 leading-tight focus:outline-none" placeholder="Password">
-      </form-group>
-      <button class="rounded hover:shadow-lg font-bold px-5 py-2 font-nunito bg-dark flex items-center">
-        Login
-      </button>
+      <form @submit.prevent="submitLogin">
+        <form-group
+          label="Username"
+        >
+          <input v-model="form.username" type="text" class="bg-dark appearance-none rounded w-full py-2 px-4 text-gray-100 leading-tight focus:outline-none" placeholder="Username">
+        </form-group>
+        <form-group
+          label="Password"
+        >
+          <input v-model="form.password" type="password" class="bg-dark appearance-none rounded w-full py-2 px-4 text-gray-100 leading-tight focus:outline-none" placeholder="Password">
+        </form-group>
+        <button class="rounded hover:shadow-lg font-bold px-5 py-2 font-nunito bg-dark flex items-center">
+          Login
+        </button>
+      </form>
     </base-card>
   </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      form: {
-        username: '',
-        password: '',
-      }
+import { mapActions } from 'vuex'
+export default {
+  data: () => ({
+    form: {
+      username: '',
+      password: '',
+    }
+  }),
+  methods: {
+    ...mapActions({
+      login: 'auth/login'
     }),
+    async submitLogin() {
+      try {
+        await this.login({ username: this.form.username, password: this.form.password })
+        this.$swal(
+          'Success',
+          'Login Success',
+          'success'
+        )
+        this.$router.push({ name: 'dashboard' })
+      } catch (error) {
+        this.$swal(
+          'Error',
+          'Login Failed',
+          'error'
+        )
+      }
+    }
   }
+}
 </script>
