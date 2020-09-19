79a77d31447b5d09e2a09b9346c79c19b17c233d
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-20 00:22:39 +0700

    config postcss again

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,19 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const postcss = require('@fullhuman/postcss-purgecss')
+
+const purgecss = postcss({
+  // Specify the paths to all of the template files in your project.
+  content: [
+      './public/**/*.html',
+      './src/**/*.vue',
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/],
+});
 
 module.exports = {
   plugins: [
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
-    purgecss({
-      content: [
-        './src/**/*.vue',
-        './public/**/*.html'
-      ],
-      whitelistPatternsChildren: [/^blueimp.+/]
-    })
+    [purgecss]
   ],
 }
