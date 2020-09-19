1e882ec2970d5d15c689ac8d1cbc5f773dc945d0
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-20 00:43:39 +0700

    purgecss lagi

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './public/**/*.html',
+    './src/**/*.vue',
+    './src/plugins/plugins.js'
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/],
+})
+
 module.exports = {
   plugins: [
     require("tailwindcss")("./tailwind.config.js"),
