75812f0919a9d8bb03ad2d6d4377678600faee06
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 17:52:32 +0700

    register page header component

--- src/plugins/globalComponent.js
+++ src/plugins/globalComponent.js
@@ -3,3 +3,4 @@ import Vue from 'vue'
 Vue.component('base-card', () => import(/* webpackChunkName: 'card-component' */'../components/Card.vue'))
 Vue.component('form-group', () => import(/* webpackChunkName: 'form-group' */'../components/FormGroup.vue'))
 Vue.component('base-pagination', () => import(/* webpackChunkName: 'pagination-componwnt' */'../components/Pagination.vue'))
+Vue.component('page-header', () => import(/* webpackChunkName: 'page-header' */'../components/PageHeader.vue'))
