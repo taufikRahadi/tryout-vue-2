d1a57070fce535c465a911c20264941079d87a6f
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 09:24:35 +0700

    add btn disable state

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="mt-5 flex justify-end w-full">
-    <button class="dark-btn" @click="prevPage">
+    <button :class="`${currentPage == 1 ? 'cursor-default' : ''} dark-btn`" :disabled="currentPage == 1" @click="prevPage">
       <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
     </button>
     <button
-      :class="currentPage == i ? 'half-dark-btn' : 'dark-btn'"
+      :class="currentPage == i ? 'half-dark-btn cursor-default' : 'dark-btn'"
+      :disabled="currentPage == i"
       v-for="i in totalPages"
       :key="i"
       @click="goTo(i)"
@@ -12,8 +13,9 @@
       {{ i }}
     </button>
     <button
-      class="dark-btn"
+      :class="`${currentPage == totalPages ? 'cursor-default' : ''} dark-btn`"
       @click="nextPage"
+      :disabled="currentPage == totalPages"
     >
       <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
     </button>
