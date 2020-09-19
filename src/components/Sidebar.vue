2b5430870febd057e474c8a014968f2e356b00fb
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 17:53:01 +0700

    add print report link into sidebar item

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -23,6 +23,10 @@
           <svg class="mr-5 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
           Users
         </router-link>
+        <router-link :to="{ name: 'print report' }" class="sidebar-link">
+          <svg class="mr-5 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+          Print Report
+        </router-link>
       </div>
     </div>
   </div>
