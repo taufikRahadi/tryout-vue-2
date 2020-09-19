aa73330705e20420134157cef02961c5acbdf0e3
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 08:26:57 +0700

    change logout btn-class

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
           </router-link>
           <svg @click="$store.commit('setShowSidebar')" class="cursor-pointer ml-10 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
         </div>
-        <button @click="logoutButton()" class="rounded hover:shadow-lg font-bold px-5 py-2 font-nunito bg-half flex items-center">
+        <button @click="logoutButton()" class="dark-btn">
           <svg class="mr-2 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
           Logout
         </button>
