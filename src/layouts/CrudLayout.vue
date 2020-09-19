2140baec4f2f77c64ad46681b1b0cd6c4e04c09f
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 17:53:24 +0700

    using page header component

--- src/layouts/CrudLayout.vue
+++ src/layouts/CrudLayout.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
     <zoom-y-transition group>
-      <div key="head" class="flex py-3 items-center justify-between w-full font-nunito">
-        <h3 class="text-3xl font-black text-gray-100 capitalize">
-          {{ $route.name }}
-        </h3>
-        <div class="flex text-white font-nunito text-light">
-          <router-link class="text-gray-300 hover:text-gray-100" to="/">
-            Dashboard
-          </router-link>
-          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-          <span class="capitalize">
-            {{ $route.name }}
-          </span>
-        </div>
-      </div>
+      <page-header key="header"/>
 
       <base-card
         key="table-card"
