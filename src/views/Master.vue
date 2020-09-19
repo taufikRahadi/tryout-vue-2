09cbf0680775a42692a510aa9ac96a6136b53556
taufikRahadi <taufikrahadi1@gmail.com>
2020-09-19 08:12:14 +0700

    refactor master layout using flex

--- src/views/Master.vue
+++ src/views/Master.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="pb-5">
     <base-header />
-    <div class="grid grid-cols-12">
-      <div class="col-span-2">
-        <base-sidebar />
-      </div>
-      <div class="col-span-10 px-10">
+    <div class="flex">
+      <slide-x-left-transition>
+        <base-sidebar v-show="$store.state.showSidebar" class="transition-all duration-400 max-w-md"/>
+      </slide-x-left-transition>
+      <div class="px-10 w-full">
         <router-view></router-view>
       </div>
     </div>
@@ -15,10 +15,12 @@
 <script>
 import Sidebar from '@/components/Sidebar.vue'
 import Header from '@/components/Header.vue'
+import { SlideXLeftTransition } from 'vue2-transitions'
 export default {
   components: {
     'base-sidebar': Sidebar,
-    'base-header': Header
+    'base-header': Header,
+    SlideXLeftTransition,
   }
 }
 </script>
